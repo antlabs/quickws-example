9aa7db181c3ac82c5873e718c8c2e96c48a0d356
guonaihong <guonaihong@qq.com>
2023-09-01 13:52:13 +0800

    up

--- Makefile
+++ Makefile
@@ -3,3 +3,4 @@ all:
 	go build -o gin-ws.client ./gin-upgrade/client/client.go
 	go build -o net-http-ws.server ./net-http-upgrade/server/server.go
 	go build -o net-http-ws.client ./net-http-upgrade/client/client.go
+	go build -o with-html.server ./with-html/server/server.go
---
+++ with-html/server/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.Run(":8081")
+}
