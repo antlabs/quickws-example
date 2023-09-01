11945dbe97a00f6dd1d4ee6dd8d90980a2f60d70
guonaihong <guonaihong@qq.com>
2023-09-01 13:56:51 +0800

    优化下demo

--- with-html/server/server.go
+++ with-html/server/server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/antlabs/quickws"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,5 +17,32 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	router.Run(":8081")
+	router.GET("/ws", func(c *gin.Context) {
+		con, err := quickws.Upgrade(c.Writer, c.Request, quickws.WithServerCallback(&handler{}))
+		if err != nil {
+			return
+		}
+		con.StartReadLoop()
+	})
+	router.Run()
+}
+
+type handler struct{}
+
+func (h *handler) OnOpen(c *quickws.Conn) {
+	fmt.Printf("服务端收到一个新的连接")
+}
+
+func (h *handler) OnMessage(c *quickws.Conn, op quickws.Opcode, msg []byte) {
+	// 如果msg的生命周期不是在OnMessage中结束，需要拷贝一份
+	// newMsg := makc([]byte, len(msg))
+	// copy(newMsg, msg)
+
+	fmt.Printf("收到客户端消息:%s\n", msg)
+	c.WriteMessage(op, msg)
+	// os.Stdout.Write(msg)
+}
+
+func (h *handler) OnClose(c *quickws.Conn, err error) {
+	fmt.Printf("服务端连接关闭:%v\n", err)
 }
